79d484e447bb6a4c815a04a8ed8ecfe91f4a7d27
gohyenther <goh.yenther@gmail.com>
2023-03-09 19:53:58 +0800

    graphql, internal DB in server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,80 @@
+const fs = require('fs');
 const express = require('express');
-
+const { ApolloServer } = require('apollo-server-express');
+const { GraphQLScalarType } = require('graphql');
+const { Kind } = require('graphql/language');
 const app = express();
 
-app.use(express.static('public'));
-
-app.listen(3000, function () {
-	  console.log('App started on port 3000');
+const GraphQLDate = new GraphQLScalarType({
+	name: 'GraphQLDate',
+	description: 'A Date() type in GraphQL as a scalar',
+	serialize(value) {
+	  return value.toISOString();
+	},
+	parseValue(value) {
+	  const dateValue = new Date(value);
+	  return isNaN(dateValue) ? undefined : dateValue;
+	},
+	parseLiteral(ast) {
+	  if (ast.kind == Kind.STRING) {
+		const value = new Date(ast.value);
+		return isNaN(value) ? undefined : value;
+	  }
+	},
 });
+
+
+/* Internal DB in server.js */
+const issueDB = [
+	{
+		id: 1,
+		status: 'New',
+		owner: 'Ravan',
+		effort: 5,
+		created: new Date('2018-08-15'),
+		due: undefined,
+		title: 'Error in console when clicking Add',
+	},
+	{
+		id: 2,
+		status: 'Assigned',
+		owner: 'Eddie',
+		effort: 14,
+		created: new Date('2018-08-16'),
+		due: new Date('2018-08-30'),
+		title: 'Missing bottom border on panel',
+	}
+]
+
+
+/* Resolvers */
+const resolvers = {
+	Query: {
+		issueList,
+	},
+	Mutation: {
+		issueAdd,
+	},
+	GraphQLDate,
+}
+
+
+/* Functions in resolvers */
+function issueList() {
+	return issueDB;
+}
+
+function issueAdd(_, {newIssue}) {
+	newIssue.id = issueDB.length + 1;
+	newIssue.created = new Date();
+	if (newIssue.status == undefined) newIssue.status = 'New';
+	issueDB.push(newIssue);
+
+	return newIssue;
+}
+
+
+const server = new ApolloServer({ typeDefs: fs.readFileSync('./server/schema.graphql', 'utf-8'), resolvers });
+app.use(express.static('public'));
+server.applyMiddleware({ app, path: '/graphql' });
+app.listen(3000, function () { console.log('App started on port 3000'); });
--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-const initialIssues = [
-	{
-		id: 1,
-		status: 'New',
-		owner: 'Ravan',
-		effort: 5,
-		created: new Date('2018-08-15'),
-		due: undefined,
-		title: 'Error in console when clicking Add'
-	},
-	{
-		id: 2,
-		status: 'Assigned',
-		owner: 'Eddie',
-		effort: 14,
-		created: new Date('2018-08-16'),
-		due: new Date('2018-08-30'),
-		title: 'Missing bottom border on panel'
-	}
-]
+const dateRegex = new RegExp('^\\d\\d\\d\\d-\\d\\d-\\d\\d');
+
+function jsonDateReviver(key, value) {
+	if (dateRegex.test(value)) return new Date(value);
+	return value;
+}
+
+
 
 class IssueFilter extends React.Component {
 	render() {
@@ -74,7 +62,8 @@ class IssueAdd extends React.Component {
 		e.preventDefault();
 		const form = document.forms.issueAdd;
 		const newIssue = {
-			owner: form.owner.value, title: form.title.value, status: 'New',
+			owner: form.owner.value, title: form.title.value,
+			due: new Date(new Date().getTime() + 1000*60*60*24*10),
 		}
 		this.props.createIssue(newIssue);
 		// reset the form input fields
@@ -103,18 +92,44 @@ class IssueList extends React.Component {
 		this.loadData();
 	}
 
-	loadData() {
-		setTimeout(() => {
-			this.setState({ issues: initialIssues })
-		}, 500);
+	async loadData() { // async: unpredictable, await present inside function
+		const query = `query {
+			issueList {
+				id
+				title
+				status
+				owner
+				effort
+				created
+				due
+			}
+		}`;
+
+		const response = await fetch('http://localhost:3000/graphql', { // await for fetch from URL
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ query })
+		});
+
+		const body = await response.text(); // await for response
+		const res = JSON.parse(body, jsonDateReviver);
+		this.setState({ issues: res.data.issueList });
 	}
 
-	createIssue(newIssue) {
-		newIssue.id = this.state.issues.length + 1;
-		newIssue.created = new Date();
-		const newIssueList = this.state.issues.slice();
-		newIssueList.push(newIssue);
-		this.setState({ issues: newIssueList });
+	async createIssue(newIssue) { // async: unpredictable, await present inside function
+		const query = `mutation issueAdd($newIssue: IssueInput!) {
+			issueAdd(newIssue: $newIssue) {
+				id
+			}
+		}`;
+
+		const response = await fetch('http://localhost:3000/graphql', { // await for fetch from URL
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ query, variables: { newIssue } })
+		});
+
+		this.loadData();
 	}
 
 	render() {
